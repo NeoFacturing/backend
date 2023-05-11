ea0dfc7a850e6914764faa5cdb4611626d53fd35
Richard <richard.kruemmel@code.berlin>
2023-05-11 15:48:25 +0200

    Fix initialisation of vectorstore

--- app/utils/vector_store.py
+++ app/utils/vector_store.py
@@ -1,19 +1,12 @@
 import os
 from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
-from app.utils.pinecone_client import pinecone_client
+
 
 def make_vectorstore() -> Pinecone:
     pinecone_index_name = os.environ.get("PINECONE_INDEX_NAME")
-    pinecone_name_space = os.environ.get("PINECONE_NAME_SPACE")
-    index = pinecone_client.Index(pinecone_index_name)
     embeddings = OpenAIEmbeddings()
-
-    vector_store = Pinecone.from_existing_index(
-        index_name=index,
-        embedding=embeddings,
-        text_key='text',
-        namespace=pinecone_name_space
+    vectorstore = Pinecone.from_existing_index(
+        index_name=pinecone_index_name, embedding=embeddings
     )
-    return vector_store
-
+    return vectorstore
