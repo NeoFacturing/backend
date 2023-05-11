d2d6ae43377f3a839ad60cba761cd96d165cd63c
Richard <richard.kruemmel@code.berlin>
2023-05-12 01:00:20 +0200

    Add authenitfication

--- app/main.py
+++ app/main.py
@@ -7,12 +7,27 @@ from fastapi import (
     Path,
     UploadFile,
 )
+import os
+from pydantic import BaseModel
+from fastapi import FastAPI, Depends, HTTPException, Query, BackgroundTasks, Path, status, Response
 from fastapi.middleware.cors import CORSMiddleware
+
+import requests
+from jose import jwt, JWTError
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
 
 from app.core.config import settings
 from app.core.chat import get_response
 from app.core.upload_data import upload_data
 from app.core.whisper import speech_to_text
+from app.utils.upload_data import upload_data
+from app.database.database import Session
+from app.database.get_user import get_user
+from app.utils.hashing import Hasher
+from app.database.schemas import Token, User
+from app.utils.bearer import OAuth2PasswordBearerWithCookie
+from app.security import create_access_token
 
 
 def get_application():
@@ -29,13 +44,81 @@ def get_application():
 
     return _app
 
-
 app = get_application()
 
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def authenticate_user(username: str, password: str, db: Session):
+    print(username)
+    user = get_user(username=username, db=db)
+    if not user:
+        return False
+    if not Hasher.verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+@app.post("/token", response_model=Token)
+def login_for_access_token(
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    access_token_expires = timedelta(
+        minutes=int(os.environ["ACCESS_TOKEN_EXPIRE_MINUTES"])
+    )
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    response.set_cookie(
+        key="access_token", value=f"Bearer {access_token}", httponly=True
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/login/token")
+
+
+def get_current_user_from_token(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token, os.environ["SECRET_KEY"], algorithms=[os.environ["ALGORITHM"]]
+        )
+        username: str = payload.get("sub")
+        print("username/email extracted is ", username)
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=username, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
 
 @app.get("/")
 async def read_root(name: Optional[str] = "World"):
     return {"Hello": name}
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_user_from_token)):
+    return current_user
 
 
 @app.get(
