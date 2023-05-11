c6866dd509fa6511be6bd65a8c1642e5d6557939
Richard <richard.kruemmel@code.berlin>
2023-05-12 00:58:02 +0200

    Add database

--- app/database/database.py
+++ app/database/database.py
@@ -1,11 +1,12 @@
-""" from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String, Integer
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URI, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(settings.DATABASE_URL(), pool_pre_ping=True)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 @as_declarative()
@@ -14,4 +15,3 @@ class Base:
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
- """
