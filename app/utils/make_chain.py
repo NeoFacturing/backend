f8f565bfc4d7d71415e5647eb042dcb67e6bcffd
Richard <richard.kruemmel@code.berlin>
2023-05-11 15:46:47 +0200

    Add sources

--- app/utils/make_chain.py
+++ app/utils/make_chain.py
@@ -1,10 +1,7 @@
-from langchain.chains import RetrievalQA, LLMChain
-from app.core.ai_model import gpt3
+from langchain.chains import RetrievalQAWithSourcesChain, LLMChain
+from langchain.llms import OpenAI
+from app.core.ai_model import chatgpt
 from typing import Any
-
-CONDENSE_PROMPT = "Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. Chat History: {chat_history} Follow Up Input: {question} Standalone question:"
-
-QA_PROMPT = "You are a helpful AI assistant that answers in german. Use the following pieces of context to answer the question at the end in german. If you don't know the answer, just say you don't know in german. DO NOT try to make up an answer. If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context in german. {context} Question: {question} Helpful answer in markdown:"
 
 
 def make_chain(vectorstore: Any) -> LLMChain:
@@ -15,8 +12,8 @@ def make_chain(vectorstore: Any) -> LLMChain:
         return None
 
     try:
-        chain = RetrievalQA.from_chain_type(
-            llm=gpt3,
+        chain = RetrievalQAWithSourcesChain.from_chain_type(
+            llm=OpenAI(),
             chain_type="stuff",
             retriever=retriever,
         )
@@ -25,4 +22,3 @@ def make_chain(vectorstore: Any) -> LLMChain:
         return None
 
     return chain
-
