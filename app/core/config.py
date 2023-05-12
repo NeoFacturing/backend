4fb4caacc27fde6464d9c821bf108570a30e8601
Mark Witt <mwitt@newstore.com>
2023-05-12 13:43:45 +0200

    Added Flutter debug URL to CORS middleware

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,9 @@ from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn, validator
 
 class Settings(BaseSettings):
     PROJECT_NAME: str
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
+        "http://localhost:58357"
+    ]
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
