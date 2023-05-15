ba6c7d2b69ee450bb3b2a42a9ce686d78c128800
Richard <richard.kruemmel@code.berlin>
2023-05-15 14:25:19 +0200

    Return retriever instead of vectorstore

--- app/utils/vector_store.py
+++ app/utils/vector_store.py
@@ -3,10 +3,11 @@ from langchain.vectorstores import Pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 
 
-def make_vectorstore() -> Pinecone:
+def make_retriever():
     pinecone_index_name = os.environ.get("PINECONE_INDEX_NAME")
     embeddings = OpenAIEmbeddings()
     vectorstore = Pinecone.from_existing_index(
         index_name=pinecone_index_name, embedding=embeddings
     )
-    return vectorstore
+    retriever = vectorstore.as_retriever()
+    return retriever
