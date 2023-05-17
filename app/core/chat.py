9d61242f6e57758aa8cf02a18452c1e2aa06421d
Richard <richard.kruemmel@code.berlin>
2023-05-17 16:31:24 +0200

    Create if statement to check if chatting with qa_chain or agent

--- app/core/chat.py
+++ app/core/chat.py
@@ -1,10 +1,8 @@
 from app.core.agent import agent_chain
-from app.utils.make_chain import make_chain
-
-# from app.utils.vector_store import make_vectorstore
+from app.core.llm_chain import qa_chain
 
 
-def get_response(input: str, history: str) -> str:
+def get_response(input: str, ai: str) -> str:
     """
     Get a response from the AI model based on the user input.
 
@@ -21,26 +19,13 @@ def get_response(input: str, history: str) -> str:
     except Exception as e:
         print(f"Error in sanitizing the input: {str(e)}")
         return None
-
-    """ try:
-        vectorstore = make_vectorstore()
-    except Exception as e:
-        print(f"Error in creating the vector store: {str(e)}")
-        return None
-
     try:
-        chain = make_chain(vectorstore)
-    except Exception as e:
-        print(f"Error in creating the chain: {str(e)}")
-        return None """
-
-    try:
-        agent_response = agent_chain.run(input=sanitizedInput)
-        # response = chain({"question": sanitizedInput, "chat_history": history})
-        # answer = response["answer"].strip().replace("\n", " ")
-        # source = response["sources"]
-        return agent_response
-        # return {"answer": answer, "source": source}
+        if ai == "agent":
+            agent_response = agent_chain.run(input=sanitizedInput)
+            return agent_response
+        else:
+            response = qa_chain.run(sanitizedInput)
+            return response
 
     except Exception as e:
         print(f"Error in running the chain: {str(e)}")
