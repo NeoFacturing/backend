af2a7feb29efa28bde9cae1ba845e42309b17918
Richard <richard.kruemmel@code.berlin>
2023-07-19 14:55:01 +0200

    Remove depreciated as_declarative

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,7 @@
+import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, String, Integer
 
 from app.database.config import settings
 
@@ -9,7 +9,7 @@ engine = create_engine(settings.DATABASE_URL(), pool_pre_ping=True)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-@as_declarative()
+@sqlalchemy.orm.as_declarative()
 class Base:
 
     @declared_attr
