a2f6d04dd8cb219c1a459fddbc80997b50974c64
Richard <richard.kruemmel@code.berlin>
2023-07-20 13:25:10 +0200

    Delete Database keys

--- app/database/database.py
+++ app/database/database.py
@@ -11,7 +11,6 @@ Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 @sqlalchemy.orm.as_declarative()
 class Base:
-
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
