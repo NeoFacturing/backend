ea184a4b0bc83c5cf6634f53641cd945969ce409
Richard <richard.kruemmel@code.berlin>
2023-07-27 15:20:17 +0200

    Run linter

--- app/core/llm.py
+++ app/core/llm.py
@@ -5,7 +5,7 @@ from langchain.chat_models import AzureChatOpenAI
 
 load_dotenv()
 openai.api_type = "azure"
-openai.api_base = os.getenv('OPENAI_API_BASE')
+openai.api_base = os.getenv("OPENAI_API_BASE")
 openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_version = os.getenv('OPENAI_API_VERSION')
+openai.api_version = os.getenv("OPENAI_API_VERSION")
 chatgpt = AzureChatOpenAI(deployment_name="neofacturing-gpt-35-turbo")
